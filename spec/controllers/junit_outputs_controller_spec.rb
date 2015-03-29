efa86703d380a84458192bfdcc5b7d560f1f256c
Aaron Humerickhouse <aaron.humerickhouse@sportngin.com>
2015-03-29 01:10:26 -0500

    Adding happy test

--- spec/controllers/junit_outputs_controller_spec.rb
+++ spec/controllers/junit_outputs_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe JunitOutputsController, type: :controller do
-
+  describe 'actions' do
+    describe 'create' do
+      it 'returns 200' do
+        post :create, format: :json, 'junit_output' => {'output' => '<xml blob>'}
+        expect(response.status).to eq 200
+        expect(JunitOutput.last.output).to eq '<xml blob>'
+      end
+    end
+  end
 end
+
+
